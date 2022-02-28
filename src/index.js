b5d2db8f1ac46f7507d787b8db2e526a142400f8
Sandeep Nagireddy <92867906+s1deepnag@users.noreply.github.com>
2022-02-28 15:28:13 -0500

    add Provider & connect to use searchField action & reducer

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
 import App from './containers/App'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import { searchRobots } from './reducers';
 //import reportWebVitals from './reportWebVitals';
 
+const store = createStore(searchRobots);
 
 ReactDOM.render(
   <React.StrictMode>
-    < App />
+    <Provider store={store}>
+      < App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
